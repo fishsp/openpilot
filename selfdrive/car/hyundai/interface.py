3cc4b915ee1d91b90ef6f6e081595c73ffe6cbdb
openpilot <5604101+axi82@user.noreply.gitee.com>
2025-03-20 16:12:06 +0800

    修改ret.startAccel = 0.8

--- selfdrive/car/hyundai/interface.py
+++ selfdrive/car/hyundai/interface.py
@@ -108,7 +108,7 @@ class CarInterface(CarInterfaceBase):
     ret.stoppingControl = True
     ret.startingState = True
     ret.vEgoStarting = 0.1
-    ret.startAccel = 0.6
+    ret.startAccel = 0.8
     ret.longitudinalActuatorDelayLowerBound = 0.5
     ret.longitudinalActuatorDelayUpperBound = 0.5
 
