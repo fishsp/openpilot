5a60bee799ede7dbbdf548c5dbd88b16d09002f7
fishsp <hlx821128@qq.com>
2025-04-05 19:49:07 +0800

    临时关闭转弯检测函数

--- selfdrive/controls/lib/longitudinal_planner.py
+++ selfdrive/controls/lib/longitudinal_planner.py
@@ -191,16 +191,16 @@ class LongitudinalPlanner:
 
     # === 新增：判断是否即将转弯 ===
     model = sm['modelV2']
-    turn_info = self.compute_turn_score(model)
-    self.is_turning = turn_info['is_turning']
-    self.turn_score = turn_info['score']
+    #turn_info = self.compute_turn_score(model)
+    #self.is_turning = turn_info['is_turning']
+    #self.turn_score = turn_info['score']
 
-    if self.is_turning:
-      print(f"[LongPlanner] turn_score: {self.turn_score:.2f}, heading_change: {np.degrees(turn_info['max_heading_change']):.1f}°, lateral_offset: {turn_info['max_lateral_offset']:.2f}m")
+    #if self.is_turning:
+    #  print(f"[LongPlanner] turn_score: {self.turn_score:.2f}, heading_change: {np.degrees(turn_info['max_heading_change']):.1f}°, lateral_offset: {turn_info['max_lateral_offset']:.2f}m")
 
       # 转弯评分大为0.4时，切换为 blended 模式
-      if self.turn_score > 0.4:
-        self.disable_carrot = True #转弯时关闭carrot功能，切换到sp的DEC自动模式
+      #if self.turn_score > 0.4:
+        #self.disable_carrot = True #转弯时关闭carrot功能，切换到sp的DEC自动模式
     # === 新增：判断是否即将转弯 ===
 
     #carrot
